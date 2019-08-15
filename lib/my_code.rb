5f8ebc444cf1c3f6d4b3f654525341329595e14c
Zachary Flowers <zflowers0@gmail.com>
2019-08-15 19:18:01 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,23 @@
 # Your Code Here
+def map(arr)
+  new_arr = []
+  arr.each do |i|
+    new_arr.push(yield(i))
+  end
+  new_arr
+end
+
+def reduce(arr, start=nil)
+  i = 0
+  if start
+    total = start
+  else
+    total = arr[0]
+    i = 1
+  end
+  while i < arr.length do
+    total = yield(total, arr[i])
+    i += 1
+  end
+  total
+end
